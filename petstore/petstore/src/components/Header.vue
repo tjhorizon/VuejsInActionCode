6ea383cf95139de916f0a6a19a4e3b1235a3b370
tjhorizon <vincentqiao2003@163.com>
2021-01-08 14:19:48 +0800

    VUE-CLI

---
+++ petstore/petstore/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <div class="navbar navbar-default">
+            <div class="navbarheader">
+                <h1><router-link :to="{name: 'iMain'}"> 
+                {{ sitename }}
+                </router-link>
+                </h1>
+            </div>
+            <div class="nav navbar-nav navbar-right cart">
+                <button type="button" class="btn btn-default btn-lg"
+                v-on:click="showCheckout">
+                <span class="glyphicon glyphicon-shopping-cart">
+                {{cartItemCount}}</span> Checkout
+                </button>
+            </div>
+        </div>
+    </header>
+</template>
---
+++ petstore/petstore/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+//import HelloWorld from '@/components/HelloWorld'
+import Form from '@/components/Form'
+import Main from '@/components/Main'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/',
+      name: 'iMain',
+      component: Main,
+      props: true
+    },
+    {
+      path:'/form',
+      name:'/Form',
+      component: Form,
+      props: true
+
+    }
+  ]
+})
