f7a3f8bf787793e9368bacd7dfdac21825e6240b
tjhorizon <vincentqiao2003@163.com>
2021-01-08 15:52:31 +0800

    20210108

--- petstore/petstore/src/components/Header.vue
+++ petstore/petstore/src/components/Header.vue
@@ -2,7 +2,7 @@
     <header>
         <div class="navbar navbar-default">
             <div class="navbarheader">
-                <h1><router-link :to="{name: 'iMain'}"> 
+                <h1><router-link :to="{name:'iMain'}"> 
                 {{ sitename }}
                 </router-link>
                 </h1>
@@ -10,10 +10,42 @@
             <div class="nav navbar-nav navbar-right cart">
                 <button type="button" class="btn btn-default btn-lg"
                 v-on:click="showCheckout">
-                <span class="glyphicon glyphicon-shopping-cart">
-                {{cartItemCount}}</span> Checkout
+                    <span class="glyphicon glyphicon-shopping-cart">
+                    {{cartItemCount}}
+                    </span>
+                Checkout
                 </button>
             </div>
         </div>
     </header>
 </template>
+
+<script>
+export default {
+  name: 'my-header',
+  data () {
+    return {
+    sitename: "Vue.js Pet Depot"
+    }
+  },
+  props: ['cartItemCount'],
+  methods: {
+    showCheckout() {
+      this.$router.push({name: 'Form'});
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+
+.router-link-exact-active {
+  color: blue;
+}
+</style>
--- petstore/petstore/src/router/index.js
+++ petstore/petstore/src/router/index.js
@@ -1,26 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-//import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
 import Form from '@/components/Form'
 import Main from '@/components/Main'
 
 Vue.use(Router)
 
 export default new Router({
-  mode:'history',
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'iMain',
-      component: Main,
-      props: true
-    },
-    {
-      path:'/form',
-      name:'/Form',
-      component: Form,
-      props: true
-
-    }
+  {
+  path: '/',
+  name: 'iMain',
+  component: Main,
+  props: true
+  },
+  {
+  path: '/form',
+  name: 'Form',
+  component: Form,
+  props: true
+  }
   ]
 })
